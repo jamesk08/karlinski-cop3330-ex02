ee94769cd172f5eb93282b9c6b5ffcb3dbc4fcdd
jamesk08 <jameskarlinski@gmail.com>
2021-08-29 11:04:29 -0400

    converting java project to maven project

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,12 +1,15 @@
-/*
- *  UCF COP3330 Fall 2021 Assignment 1 Solution
- *  Copyright 2021 James Karlinski
- */
+package org.example;
 
 import java.util.Scanner;
 
-public class CharacterCounter {
-    public static void main(String[] args) {
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
         System.out.print("What is the input string? ");
         Scanner scanner = new Scanner(System.in);
         String response = scanner.nextLine();
