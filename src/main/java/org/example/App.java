8c9197d56419cdc941340dfcd12a6276db2f3ddd
jamesk08 <jameskarlinski@gmail.com>
2021-08-29 11:25:02 -0400

    adding missing author comments

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -2,9 +2,9 @@ package org.example;
 
 import java.util.Scanner;
 
-/**
- * Hello world!
- *
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 James Karlinski
  */
 public class App 
 {
